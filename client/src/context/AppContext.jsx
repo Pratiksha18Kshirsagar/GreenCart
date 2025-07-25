c8b11e6fe3017f7163497640ca7ef1b7659c7654
Pratiksha18Kshirsagar <kshirsagarpratiksha1820@gmail.com>
2025-07-25 22:41:27 +0530

    first commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,83 @@
+
+import { createContext, useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { dummyProducts } from '../assets/assets';
+import toast from 'react-hot-toast';
+
+
+// 1️⃣ Create the context
+export const AppContext = createContext();
+
+
+// 2️⃣ Create a provider component
+export const AppContextProvider = ({ children }) => {
+    const currency = import.meta.VITE_CURRENCY;    
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [cartItems, setCartItems] = useState({});
+
+
+    //fetch all products
+    const fetchProducts = async () => {
+        setProducts(dummyProducts);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+
+    //Add products to cart
+    const addToCart = (itemId) => {
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            cartData[itemId] += 1;
+        } else {
+            cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+        toast.success("Added to cart")
+    }
+
+    //update cart item quantity
+    const updateCartItem = (itemId, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId] = quantity;
+        setCartItems(cartData)
+        toast.success("Cart Updated")
+    }
+
+
+    //Remove item from cart
+    const removeFromCart = (itemId)=>{
+        const cartData = structuredClone(cartItems);
+        if(cartData[itemId]){
+            cartData[itemId] -= 1;
+            if(cartData[itemId] === 0){
+                delete cartData[itemId];
+            }
+        }
+        setCartItems(cartData);
+        toast.success("Reomved from cart");
+        
+
+    }
+
+    const value = { navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem ,removeFromCart , cartItems};
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+
+}
+
+
+// 3️⃣ Custom hook to use context
+export const useAppContext = () => {
+    return useContext(AppContext);
+}
+
+
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
+import Bestseller from '../components/Bestseller'
+import BottomBanner from '../components/BottomBanner'
+
+const Home = () => {
+  return (
+    <div className='mt-10'>
+      <MainBanner/>
+      <Categories/>
+      <Bestseller/>
+      <BottomBanner/>
+    </div>
+  )
+}
+
+export default Home
